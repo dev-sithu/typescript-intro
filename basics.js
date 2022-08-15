513f5c233e862c36036fd5bdbaf09485c9846c0c
Sithu <sithu@aluto-benli.com>
2022-08-15 23:05:56 +0630

    Add src and dist for rootDir and outDir

--- basics.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-(function () {
-    var username = "Sithu";
-    console.log("Hello ".concat(username));
-})();
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+// import person from './Person';
+
 function combine(a: number | string, b: number | string): number | string {
     if (typeof a === 'number' && typeof b === 'number') {
         return a + b;
@@ -6,12 +8,15 @@ function combine(a: number | string, b: number | string): number | string {
     }
 }
 
+function printResult(msg: any): void {
+    console.log(new Date() + ': ' + msg.toString());
+}
+
+// printResult(person.name);
+// printResult(person.role);
+
 const combinedNums = combine(10, 15);
 printResult(combinedNums);
 
 const combinedNames = combine('Takahiro', 'Kanki');
 printResult(combinedNames);
-
-function printResult(msg: any): void {
-    console.log(new Date() + ': ' + msg.toString());
-}
